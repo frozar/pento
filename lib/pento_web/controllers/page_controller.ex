01b9029b2e84cb158aa025f1269a37743a68c364
frozar <fabien.rozar@gmail.com>
2026-07-02 11:55:51 +0400

    chapitre 2 : implémentation de la redirection d'un utilisateur s'il est authentifié ou pas.

--- lib/pento_web/controllers/page_controller.ex
+++ lib/pento_web/controllers/page_controller.ex
@@ -2,6 +2,10 @@ defmodule PentoWeb.PageController do
   use PentoWeb, :controller
 
   def home(conn, _params) do
+  if conn.assigns.current_scope && conn.assigns.current_scope.user do
+    redirect(conn, to: ~p"/guess")
+  else
     render(conn, :home)
+  end
   end
 end
